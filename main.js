186ab70850986fe6a1e8ed766a3edf47a9018ff5
Felipesv1 <fepereirasilva4@gmail.com>
2023-05-13 14:00:51 -0300

    Desafio concluido

---
+++ main.js
@@ -0,0 +1,10 @@
+const local = document.querySelector('header p')
+
+
+setTimeout(()=> {
+    local.innerHTML = 'Chega em 1 minutos (400 metros) '
+},4000)
+
+setTimeout(()=> {
+    local.innerHTML = 'Chegou ao local de partida '
+},9000)
